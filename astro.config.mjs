f0ec12947407ef5df82f1b5fb72ada21d269db43
salema97 <salema@espe.edu.ec>
2026-02-18 02:54:07 -0500

    feat: Reinitialize project with Astro and pnpm, adding new pages, components, and assets while removing old configuration files.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,5 @@
-import tailwind from '@astrojs/tailwind'
-import { defineConfig } from 'astro/config'
-
-import react from '@astrojs/react'
+// @ts-check
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-	site: 'https://salema97.github.io',
-	integrations: [tailwind(), react()],
-	i18n: {
-		defaultLocale: 'es',
-		locales: ['es', 'en']
-	},
-	routing: {
-		prefixDefaultLocale: false
-	}
-})
+export default defineConfig({});
